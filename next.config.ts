31c1d2e5af822b5b280630beca9b795696d5dde0
Chin Trinh <trinhchinchin@gmail.com>
2025-03-30 16:03:25 +0700

    feat: add app version

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
+import { execSync } from "child_process";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    APP_VERSION: execSync("git describe --tags --always").toString().trim(),
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,6 +4,10 @@ export default function Footer() {
   return (
     <footer className="py-4 text-center text-sm text-gray-500">
       © {new Date().getFullYear()} @chinchinbooth. All rights reserved.
+      <br />
+      <span className="text-xs">
+        Version: {process.env.NEXT_PUBLIC_APP_VERSION}
+      </span>
     </footer>
   );
 }
